ed967be4d30fbaab65427ce2810bc0884b3c55e2
Tom Zellman <tzellman@gmail.com>
2015-03-23 18:32:58 -0400

    follow addon best practices by referencing the component from the addon namespace

--- addon/mixins/slide-show-controller.js
+++ addon/mixins/slide-show-controller.js
@@ -1,5 +1,10 @@
 import Ember from "ember";
 
+/**
+ * Simple mixin that adds a `slide` query parameter, which is bound to the slideIndex property.
+ *
+ * Use the slideIndex property in your templates to bind to a slide-show component.
+ */
 export default Ember.Mixin.create({
 
     queryParams: {
